4f4c2c1810713ff1ec23993cb5f5c15dbf0c5b34
yaduND <yknd80@gmail.com>
2024-07-12 19:57:49 +0530

    grey-scale done

--- main.c
+++ main.c
@@ -1,8 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-	
-	printf("hello world");
+int main()
+{
+	char path[20];
+	scanf("%s", path);
 
-	return 0;
+    FILE *fIn = fopen(path, "rb");
+    FILE *fOut = fopen("tiger_gray.bmp", "wb");
+    if (!fIn || !fOut)
+    {
+        printf("File error.\n");
+        return 0;
+    }
+
+    unsigned char header[54];
+    fread(header, sizeof(unsigned char), 54, fIn);
+    fwrite(header, sizeof(unsigned char), 54, fOut);
+
+    int width = *(int*)&header[18];
+    int height = abs(*(int*)&header[22]);
+    int stride = (width * 3 + 3) & ~3;
+    int padding = stride - width * 3;
+
+    printf("width: %d (%d)\n", width, width * 3);
+    printf("height: %d\n", height);
+    printf("stride: %d\n", stride);
+    printf("padding: %d\n", padding);
+
+    unsigned char pixel[3];
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            fread(pixel, 3, 1, fIn);
+            unsigned char gray = pixel[0] * 0.3 + pixel[1] * 0.58 + pixel[2] * 0.11;
+            memset(pixel, gray, sizeof(pixel));
+            fwrite(&pixel, 3, 1, fOut);
+        }
+        fread(pixel, padding, 1, fIn);
+        fwrite(pixel, padding, 1, fOut);
+    }
+    fclose(fOut);
+    fclose(fIn);
+    return 0;
 }
+
